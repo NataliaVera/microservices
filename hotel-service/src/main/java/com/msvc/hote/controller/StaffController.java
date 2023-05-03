e1d79d3913cba53443de8dae7a1e38110df0fa85
Natalia Andrea Vera Torres <nataliavera2100@gmail.com>
2023-05-03 11:16:11 -0500

    API Gateway

---
+++ hotel-service/src/main/java/com/msvc/hote/controller/StaffController.java
@@ -0,0 +1,22 @@
+package com.msvc.hote.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staffs")
+public class StaffController {
+
+    @GetMapping
+    public ResponseEntity<List<String>> staffList(){
+        List<String> staffs = Arrays.asList("Prueba1", "Prueba2", "Prueba3", "Prueba4", "Prueba5");
+        return new ResponseEntity<>(staffs, HttpStatus.OK);
+    }
+
+}
